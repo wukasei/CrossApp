45d1bdcafdc4e42c8068f6e28058c8ea73106a83
Inna <bohachuk.inna@gmail.com>
2026-09-04 19:56:38 +0300

    feat: add --json flag, multi-RID publish sizes and docker

--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -1,18 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Text.Json;
+using System.Text.Encodings.Web;
 
 Console.OutputEncoding = Encoding.UTF8;
 
-Console.WriteLine("CrossApp – практикум з крос-платформного програмування");
-Console.WriteLine("Студент: Інна Богачук, група 32");
-Console.WriteLine(new string('-', 52));
-Console.WriteLine($"ОС (OSDescription)  : {RuntimeInformation.OSDescription}");
-Console.WriteLine($"ОС (Environment)    : {Environment.OSVersion}");
-Console.WriteLine($"Архітектура процесу : {RuntimeInformation.ProcessArchitecture}");
-Console.WriteLine($"Версія .NET (CLR)   : {Environment.Version}");
-Console.WriteLine($"Runtime             : {RuntimeInformation.FrameworkDescription}");
-Console.WriteLine($"Каталог застосунку  : {AppContext.BaseDirectory}");
-Console.WriteLine($"Поточний каталог    : {Environment.CurrentDirectory}");
-Console.WriteLine(new string('-', 52));
-Console.WriteLine("Предметна область: Бібліотека (видання, примірник, читач, видача)");
+bool isJson = args.Contains("--json");
+
+var sysInfo = new
+{
+    Student = "Інна Богачук",
+    Group = "32",
+    OSDescription = RuntimeInformation.OSDescription,
+    OSVersion = Environment.OSVersion.ToString(),
+    ProcessArchitecture = RuntimeInformation.ProcessArchitecture.ToString(),
+    DotNetVersion = Environment.Version.ToString(),
+    Runtime = RuntimeInformation.FrameworkDescription,
+    AppBaseDirectory = AppContext.BaseDirectory,
+    CurrentDirectory = Environment.CurrentDirectory,
+    Domain = "Бібліотека (видання, примірник, читач, видача)"
+};
+
+if(isJson){
+    var options = new JsonSerializerOptions
+    {
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+    };
+    Console.WriteLine(JsonSerializer.Serialize(sysInfo, options));
+}
+else{
+    Console.WriteLine("CrossApp – практикум з крос-платформного програмування");
+    Console.WriteLine("Студент: Інна Богачук, група 32");
+    Console.WriteLine(new string('-', 52));
+    Console.WriteLine($"ОС (OSDescription)  : {RuntimeInformation.OSDescription}");
+    Console.WriteLine($"ОС (Environment)    : {Environment.OSVersion}");
+    Console.WriteLine($"Архітектура процесу : {RuntimeInformation.ProcessArchitecture}");
+    Console.WriteLine($"Версія .NET (CLR)   : {Environment.Version}");
+    Console.WriteLine($"Runtime             : {RuntimeInformation.FrameworkDescription}");
+    Console.WriteLine($"Каталог застосунку  : {AppContext.BaseDirectory}");
+    Console.WriteLine($"Поточний каталог    : {Environment.CurrentDirectory}");
+    Console.WriteLine(new string('-', 52));
+    Console.WriteLine("Предметна область: Бібліотека (видання, примірник, читач, видача)");
+}
+
